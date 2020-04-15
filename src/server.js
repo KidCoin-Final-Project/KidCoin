ad81f2c6aa77c8862bc7472bcfd175113997379f
LidorSho <LidorSho>
2020-04-15 19:13:04 +0300

    first connection to firebase

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 var express = require('express'); //import express 
 var app = express();
 
+var dbCon = require('./dbConnection')
+
 //to handle HTTP get request
 app.get('/', function (req, res) {
     console.log("Get Request");
@@ -12,6 +14,12 @@ var server = app.listen(8080, function () {
 
     var host = server.address().address;
     var port = server.address().port;
+    dbCon.database.collection('users').doc('1').get().then(doc =>{
+        console.log(doc)
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
     console.log("server listening at http://%s:%s", host, port);
 });
