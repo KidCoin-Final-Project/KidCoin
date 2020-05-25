8410fc47ab6164b732a0c3ad6cb5a6452c644d83
LidorSho <LidorSho@gmail.com>
2020-05-25 22:07:03 +0300

    added swagger

--- server/src/server.js
+++ server/src/server.js
@@ -3,11 +3,28 @@ const bodyParser = require('body-parser')
 const app = express();
 const routes = require('./routes')
 const cors = require('cors')
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 app.use(bodyParser.json())
 app.use(cors())
 
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: "kidcoin API",
+        description: "kidcoin API Information"
+      }
+    },
+    // ['.routes/*.js']
+    apis: ['../routes.js']
+  };
+
 app.use('/',routes)
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 //start server on port: 8080
 var server = app.listen(8080, function () {
