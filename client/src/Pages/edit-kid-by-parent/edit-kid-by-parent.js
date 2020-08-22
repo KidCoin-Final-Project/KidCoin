b7b1cb2a699d1039a02d1769287e11604702c0f7
shakedd20 <shakedd15@gmail.com>
2020-08-22 11:50:19 +0300

    finished Owner page

--- client/src/Pages/edit-kid-by-parent/edit-kid-by-parent.js
+++ client/src/Pages/edit-kid-by-parent/edit-kid-by-parent.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.css"
 import "../edit-kid-by-parent/edit-kid-by-parent.css"
 import TopNavBar from "../../Components/Top-Navbar/top-navbar";
+import Modal from "react-bootstrap/Modal";
 
 class EditKidByParent extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            show: false,
         };
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
 
     componentDidMount() {
+        this.setState({kids: this.props.location.state.kid})
     }
 
-    getLastActivitiesDataFromServer(childId, token) {
+    handleShow(){
+        this.state.show = true
+    }
+
+    handleClose(){
+        this.state.show = false
     }
 
     render() {
@@ -22,15 +32,16 @@ class EditKidByParent extends Component {
             <div className="remain-cash-and-options-kidAlergic">
                 <div className="remain-cash-kidAlergic">
                     <div className="cash-kidAlergic">
+                        {/*<img src={"http://localhost:8080/images/" + {this:props.location.state.kid.child.picture}} style={{ "borderRadius": "100%", "height": "7vh", "width": "7vh" }} />*/}
+                        {/*<img className="g-kidAlergic" src={"http://localhost:8080/images/" + this.props.location.state.kid.child.picture}  />*/}
                         <img className="g-kidAlergic" src="images/the-beautiful-girl-2.jpg"/>
                     </div>
                     <div>
-                        <span className="child-name-text-kidAlergic">שקד מיוחדת </span>
+                        <span className="child-name-text-kidAlergic">{this.props.location.state.kid.child.firstName}</span>
                     </div>
                     <div>
                         <span className="product-name-kidAlergic">יתרה:</span>
-                        <span className="product-name-kidAlergic">1.5</span>
-                        <span className="product-name-kidAlergic">ביטקוין</span>
+                        <span className="product-name-kidAlergic">{this.props.location.state.kid.child.balance} ש"ח </span>
                     </div>
                 </div>
             </div>
@@ -82,17 +93,32 @@ class EditKidByParent extends Component {
                     <button className="btn btn-light option-button-kidAlergic"><span>שוקולד</span><span>x</span></button>
                 </div>
                 <button className="btn btn-light add-button-kidAlergic"
-                        onClick="openModal('modal', 'close-modal-button-separate')"><span>הוספה</span><span>+</span>
+                        variant="primary" onClick={this.handleShow}><span>הוספה</span><span>+</span>
                 </button>
             </div>
 
+            <Modal show={this.state.show} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <button variant="secondary" onClick={this.handleClose}>
+                        Close
+                    </button>
+                    <button variant="primary" onClick={this.handleClose}>
+                        Save Changes
+                    </button>
+                </Modal.Footer>
+            </Modal>
+
         </div>
         </div>
+
+
     );
     }
 }
 
-
-// KidHome.contextType = userContext;
 export default EditKidByParent;
 
