7d6b58fac3c1e0e6aabf33a6552dec8a0d8381ce
LidorSho <LidorSho@gmail.com>
2020-08-22 23:03:14 +0300

    added multiple restrictions add/delete

--- server/src/dal/childDAL.js
+++ server/src/dal/childDAL.js
@@ -32,7 +32,7 @@ module.exports = {
             return db.collection('child').doc(childId).update({balance:doc.data().balance + balanceToAdd})
         })
     },
-    addRestriction: function (childId, productId){
+    addRestriction: function (childId, productIds){
         return db.collection('child').doc(childId).get().then(doc => {
             if(!doc.exists){
                 throw 404;
@@ -41,30 +41,29 @@ module.exports = {
             if(!restrictions){
                 restrictions = [];
             }
-            restrictions.push(db.collection('product').doc(productId))
+            for (let i = 0; i < productIds.length; i++) {
+                restrictions.push(db.collection('product').doc(productIds[i]))
+            }
             return db.collection('child').doc(childId).update({restrictions:restrictions})
         })
     },
-    removeRestriction: function (childId, productId){
+    removeRestriction: function (childId, productIds){
         return db.collection('child').doc(childId).get().then(doc => {
             if(!doc.exists){
                 throw 404;
             }
             var restrictions = doc.data().restrictions;
-            var index = -1
             if(!restrictions){
                 restrictions = [];
             } else {
+                newRestrictions = []
                 for (let i = 0; i < restrictions.length; i++) {
-                    if(restrictions[i].id == productId){
-                        index = i;
+                    if(productIds.indexOf(restrictions[i].id) == -1){
+                        newRestrictions.push(restrictions[i])
                     }
                 } 
             }
-            if (index > -1) {
-                restrictions.splice(index, 1);
-            }
-            return db.collection('child').doc(childId).update({restrictions:restrictions})
+            return db.collection('child').doc(childId).update({restrictions:newRestrictions})
         })
     },
     deleteChild: function(childId){
